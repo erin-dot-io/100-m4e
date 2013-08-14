6ffb63fd3de9b2c39e06f6f937559b1c602ded10
erinautomatic <edkeeffe@gmail.com>
2013-08-13 23:38:20 -0400

    password gate update

--- index.php
+++ index.php
@@ -1,17 +1,30 @@
 <?php
-$pass = $_POST['pass'];
+$password = "ahundredyears";
+$nonsense = "supercalifragilisticexpialidocious";
 
-if($pass == "ahundredyears")
-{
-        include("home.html");
+if (isset($_COOKIE['PrivatePageLogin'])) {
+   if ($_COOKIE['PrivatePageLogin'] == md5($password.$nonsense)) {
+?>
+
+    <?php include_once("home.html"); ?>
+
+<?php
+      exit;
+   } else {
+      echo "Bad Cookie.";
+      exit;
+   }
 }
-if(isset($_POST))
-{?>
 
-        <form method="POST" action="index.php">
-        User <input type="TEXT" name="user"></input>
-        Pass <input type="TEXT" name="pass"></input>
-        <input type="submit" name="submit"></input>
-        </form>
-<?}
+if (isset($_GET['p']) && $_GET['p'] == "login") {
+   if ($_POST['keypass'] != $password) {
+      echo "Incorrect password.";
+      exit;
+   } else if ($_POST['keypass'] == $password) {
+      setcookie('PrivatePageLogin', md5($_POST['keypass'].$nonsense));
+      header("Location: $_SERVER[PHP_SELF]");
+   } else {
+      echo "Whoops, something went wrong.";
+   }
+}
 ?>
