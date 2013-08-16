0aaba8eb3b254b4e48832d344ae54bc32dbb4c4a
erinautomatic <edkeeffe@gmail.com>
2013-08-16 13:58:23 -0400

    lots o stuff

--- index.php
+++ index.php
@@ -28,8 +28,21 @@ if (isset($_GET['p']) && $_GET['p'] == "login") {
    }
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta name="robots" content="noindex" />
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  <title>Compass</title>
+  <link rel="stylesheet" href="stylesheets/screen.css">
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+</head>
+<body>
 
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>?p=login" method="post">
-<label><input type="password" name="keypass" id="keypass" placeholder="Password"/></label>
-<input type="submit" id="submit" value="Submit" />
-</form>
+   <form class="gate-form" action="<?php echo $_SERVER['PHP_SELF']; ?>?p=login" method="post">
+      <label><input type="password" name="keypass" id="keypass" placeholder="Password"/></label>
+      <input type="submit" id="submit" class="btn" value="Submit" />
+   </form>
+</body>
+</html>
